2cd3190df8af3a3a4140a5f3da59b39084a3e754
Igor Novak <bezengi@gmail.com>
2012-06-01 10:14:40 +0300

    Implement slides switching

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,2 +1,41 @@
+$.fn.present = function(options) {
+  options = options || {};
+  var slides = $('article', this),
+      current = 0;
+
+  $(document).on('keydown', function(e) {
+    switch (e.which) {
+      case 33:
+      case 37:
+        prevSlide();
+        break;
+      case 34:
+      case 39:
+        nextSlide();
+    }
+  });
+  $(options.prevBtn).on('click', prevSlide);
+  $(options.nextBtn).on('click', nextSlide);
+
+  go(current);
+
+  function prevSlide() {
+    go(Math.max(0, --current));
+  };
+
+  function nextSlide() {
+    go(Math.min(slides.length - 1, ++current));
+  };
+
+  function go(n) {
+    current = n;
+    slides.hide().eq(n).show()
+  };
+}
+
 $(function() {
+  $('#presentation').present({
+    prevBtn: '.btn.prev',
+    nextBtn: '.btn.next',
+  });
 });
